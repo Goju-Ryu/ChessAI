726f16e8a27769ac322681a04e4dbb05435bf0a6
s185132 <s185132@student.dtu.dk>
2021-11-09 14:24:37 +0000

    Add preliminary state implementations

---
+++ ChessAI/piece.cs
@@ -0,0 +1,22 @@
+﻿namespace ChessAI {
+
+    public enum Pieces : byte {
+        Pawn,
+        Rook,
+        Knight,
+        Bishop,
+        Queen,
+        King
+    }
+    public readonly struct Move{
+        public readonly byte StartPos;
+        public readonly byte EndPos;
+        public Move(byte startPos , byte endPos){ StartPos = startPos; EndPos = endPos; }
+    }
+
+    // inspiration Source https://www.youtube.com/watch?v=U4ogK0MIzqk
+    public class Piece {
+
+
+    }
+}
