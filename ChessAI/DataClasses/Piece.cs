0132889cf4c5f91a566ea2f01bd726445ee48d9b
Goju Ryu <mads1606@gmail.com>
2021-11-17 20:54:11 +0100

    Add Equals method to Piece

--- ChessAI/DataClasses/Piece.cs
+++ ChessAI/DataClasses/Piece.cs
@@ -3,8 +3,26 @@ using System.Text;
 
 namespace ChessAI.DataClasses
 {
-    public readonly struct Piece //todo could this be a ref struct?
+    //todo could this be a ref struct?
+    public readonly struct Piece : IEquatable<Piece>
     {
+        /// <summary>
+        /// _piece stores information describing a single piece possibly on a board.
+        /// The byte representing the piece is conceptually split into two parts; one describing the pieces color
+        /// and type, and another describing additional information relating to the state it is in.
+        /// The piece type is represented by the three least significant bits as consecutive numbers starting at 1.
+        /// The color is represented by the next bit where a 1 is white and 0 is black.
+        ///
+        /// below is a description of the layout of _piece starting from the most significant bit
+        /// towards the least significant on
+        ///
+        ///     unused:         2 bits
+        ///     challenges:     1 bit
+        ///     is challenged:  1 bit
+        ///     is white:       1 bit
+        ///     Piece type:     3 bits
+        ///
+        /// </summary>
         private readonly byte _piece;
 
         public byte Content => _piece;
@@ -28,12 +46,12 @@ namespace ChessAI.DataClasses
         public const byte PieceMask = 0b0111;
 
         // Piece definitions
-        public const byte Pawn      = 0b0001;
-        public const byte Rook      = 0b0010;
-        public const byte Knight    = 0b0011;
-        public const byte Bishop    = 0b0100;
-        public const byte Queen     = 0b0101;
-        public const byte King      = 0b0110;
+        public const byte Pawn = 0b0001;
+        public const byte Rook = 0b0010;
+        public const byte Knight = 0b0011;
+        public const byte Bishop = 0b0100;
+        public const byte Queen = 0b0101;
+        public const byte King = 0b0110;
 
         // Flags
         public const byte White = 0b1000;
@@ -59,32 +77,46 @@ namespace ChessAI.DataClasses
                     _ => "Invalid"
                 }
             );
-            
+
             //Insert appends for other flags here as they are decided on
 
             return builder.ToString();
         }
 
+        public bool Equals(Piece other)
+        {
+            return _piece == other._piece;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Piece other && Equals(other);
+        }
+
+        public override int GetHashCode()
+        {
+            return _piece.GetHashCode();
+        }
 
         //######################################//
         //          Operator overloads          //
         //######################################//
-        
+
         // bitwise And
         public static Piece operator &(Piece a, Piece b) => new Piece((byte)(a._piece & b._piece));
         public static Piece operator &(Piece a, byte b) => new Piece((byte)(a._piece & b));
         public static Piece operator &(byte a, Piece b) => new Piece((byte)(a & b._piece));
-        
+
         // bitwise Or
         public static Piece operator |(Piece a, Piece b) => new Piece((byte)(a._piece | b._piece));
         public static Piece operator |(Piece a, byte b) => new Piece((byte)(a._piece | b));
         public static Piece operator |(byte a, Piece b) => new Piece((byte)(a | b._piece));
-        
+
         // bitwise XOr
         public static Piece operator ^(Piece a, Piece b) => new Piece((byte)(a._piece ^ b._piece));
         public static Piece operator ^(Piece a, byte b) => new Piece((byte)(a._piece ^ b));
         public static Piece operator ^(byte a, Piece b) => new Piece((byte)(a ^ b._piece));
-        
+
         // Equality
         public static bool operator ==(Piece a, Piece b) => a._piece == b._piece;
         public static bool operator ==(byte a, Piece b) => a == b._piece;
